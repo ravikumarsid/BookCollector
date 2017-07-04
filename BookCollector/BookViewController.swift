e8d5814f83548580e23480d890b28b2d89e68d9a
Ravi <ravikumarsid@gmail.com>
2017-07-04 14:53:11 -0400

    Added two views

---
+++ BookCollector/BookViewController.swift
@@ -0,0 +1,51 @@
+//
+//  BookViewController.swift
+//  BookCollector
+//
+//  Created by Ravi Kumar Venuturupalli on 7/4/17.
+//  Copyright © 2017 Ravi Kumar Venuturupalli. All rights reserved.
+//
+
+import UIKit
+
+class BookViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        bookImageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let book = Book(context: context)
+        book.title = titleTextField.text
+        book.image = UIImagePNGRepresentation(bookImageView.image!) as NSData?
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+    }
+}
